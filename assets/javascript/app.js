e4e30c4a7a17cb21b577ec45cdc577698c7f21d7
ccho0415 <ccho0415@gmail.com>
2017-01-05 23:36:53 -0500

    added fullpage js (should making the website scrolling option a lot easier)

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,33 +1,6 @@
 $(document).ready(function() {
-    $("#WeatherText").hide();
-    $("#landingText").hide();
-    $("#NewsText").hide();
-    $("#ToDoText").hide();
+    $('#fullpage').fullpage();
+}); 
   $("#test").on("click", function(){
     console.log("ouch");
   });
-  $("#news-side-nav-bar").on("click", function(){
-    $("body").removeClass();
-    $("body").addClass("news");
-    $("#WeatherText").hide();
-    $("#landingText").hide();
-    $("#NewsText").show();
-    $("#ToDoText").hide();
-  });
-  $("#to-do-side-nav-bar").on("click", function(){
-    $("body").removeClass();
-    $("body").addClass("toDo");
-    $("#WeatherText").hide();
-    $("#landingText").hide();
-    $("#NewsText").hide();
-    $("#ToDoText").show();
-  });
-  $("#weather-side-nav-bar").on("click", function(){
-    $("body").removeClass();
-    $("body").addClass("weather");
-    $("#WeatherText").show();
-    $("#landingText").hide();
-    $("#NewsText").hide();
-    $("#ToDoText").hide();
-  });
-});    
