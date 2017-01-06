b69cafca0b55a6c4af0b4babd86952621049b675
ccho0415 <ccho0415@gmail.com>
2017-01-06 10:24:29 -0500

    added the side nav bar, css and animations

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
     $('#wrapper').fullpage();
 }); 
-  $("#test").on("click", function(){
+$("#test").on("click", function(){
     console.log("ouch");
   });
+// NavBar CSS Starts
+ // Bump Page Content Over 
+function openNav() {
+  document.getElementById("sidenav").style.width = "250px";
+  document.getElementById("wrapper").style.marginLeft = "250px";
+}
+
+ // Bump Page 
+function closeNav() {
+  document.getElementById("sidenav").style.width = "0";
+  document.getElementById("wrapper").style.marginLeft = "0";
+}
+// NavBar CSS Ends
