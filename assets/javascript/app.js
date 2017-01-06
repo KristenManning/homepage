011d46e9ccb82148da255169f2f760fd05ae2a60
ccho0415 <ccho0415@gmail.com>
2017-01-05 21:27:22 -0500

    added more to the html skeleton. currently researching on how to animate the pages.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,33 @@
 $(document).ready(function() {
-  alert("Hi");
-})       
+    $("#WeatherText").hide();
+    $("#landingText").hide();
+    $("#NewsText").hide();
+    $("#ToDoText").hide();
+  $("#test").on("click", function(){
+    console.log("ouch");
+  });
+  $("#news-side-nav-bar").on("click", function(){
+    $("body").removeClass();
+    $("body").addClass("news");
+    $("#WeatherText").hide();
+    $("#landingText").hide();
+    $("#NewsText").show();
+    $("#ToDoText").hide();
+  });
+  $("#to-do-side-nav-bar").on("click", function(){
+    $("body").removeClass();
+    $("body").addClass("toDo");
+    $("#WeatherText").hide();
+    $("#landingText").hide();
+    $("#NewsText").hide();
+    $("#ToDoText").show();
+  });
+  $("#weather-side-nav-bar").on("click", function(){
+    $("body").removeClass();
+    $("body").addClass("weather");
+    $("#WeatherText").show();
+    $("#landingText").hide();
+    $("#NewsText").hide();
+    $("#ToDoText").hide();
+  });
+});    
