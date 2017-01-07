8fb7aaf14ffee2189856213de837e807044b4006
Kristen Manning <kristenm@Kristens-MacBook-Pro.local>
2017-01-07 13:41:18 -0500

    adding improved NYT and weather formatting

--- references/testingAPIs.js
+++ references/testingAPIs.js
@@ -1,4 +1,9 @@
-// New York Times Top Stories 
+// New York Times Top Stories With Multiple Topics
+// API Key = 9576deafb6a7491fb2be036f97f0b645
+
+
+
+// BASIC NYT TOP STORIES: 
 // API Key = 9576deafb6a7491fb2be036f97f0b645
 
 var topic = "opinion" // topic choices include: home, opinion, world, national, politics, business, technology, science, health, sports, arts, books, movies, theater, fashion, food, travel... and more
@@ -6,8 +11,8 @@ var nyt_url = "http://api.nytimes.com/svc/topstories/v1/"+topic+".json?api-key=9
 $.ajax({url: nyt_url, method: 'GET'}).done(function(result) {
   console.log(result);
   for (var i = 0; i <3; i++) {
-  	var current_result = result.results[i]
-  	$("#nyt").append("<b>" + current_result.title + "</b><br>" + current_result.byline + "<br><i>" + current_result.abstract + "</i><br>" + current_result.url + "<br><br>")
+    var current_result = result.results[i]
+    $("#nyt").append("<div class='row'> <div class='col-md-1'> <img src="+current_result.multimedia[0].url+"></div><div class='col-md-4'> <b> <a href="+ current_result.url + ">" + current_result.title + "</a></b><br>" + current_result.byline + "<br><i>" + current_result.abstract + "</i><br><br></div></div>")
   }
   
   }).fail(function(err) {
@@ -20,27 +25,28 @@ $.ajax({url: nyt_url, method: 'GET'}).done(function(result) {
 // API Key = 7339d8215d69c41b505afbebf91a1097
 // We could look into doing this by city ID (see Open Weather Map documentation) for more accuracy
 // This example uses city (name) and country code (initials) in the url 
-var city = "New York"
+var city = "New York, NY"
 var lat = 40.7128
 var lon = 74.0059
 var weather_url = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&mode=json&APPID=7339d8215d69c41b505afbebf91a1097"
 
 // This ajax call returns weather objects for each 3 hour block for 5 days (stored in the array of length 40)
 // Currently it only displays informaton from the first element in that array (first 3-hour block)
+
 $.ajax({url: weather_url, method: 'GET'}).done(function(result) {
   console.log(result);
   var kelvin = result.list[0].main.temp; 
   var faren = Math.round(kelvin*9/5 - 459.67);
 
-  $("#weather").append("<b>" + city + " <br><br>Now</b><br> " + result.list[0].weather[0].description + "<br>" + faren + " Degrees Farenheit <br><br>")
-  $("#weather").append("<b>In 3 Hours</b><br>" + result.list[1].weather[0].description + "<br>" + faren + " Degrees Farenheit <br><br>")
-  $("#weather").append("<b>In 6 Hours</b><br>" + result.list[2].weather[0].description + "<br>" + faren + " Degrees Farenheit <br><br>")
-  $("#weather").append("<b>In 9 Hours</b><br>" + result.list[3].weather[0].description + "<br>" + faren + " Degrees Farenheit <br><br>")
-  $("#weather").append("<b>In 12 Hours</b><br>" + result.list[4].weather[0].description + "<br>" + faren + " Degrees Farenheit <br><br>")
-  $("#weather").append("<b>In 15 Hours</b><br>" + result.list[5].weather[0].description + "<br>" + faren + " Degrees Farenheit <br><br>")
-  $("#weather").append("<b>In 18 Hours</b><br>" + result.list[6].weather[0].description + "<br>" + faren + " Degrees Farenheit <br><br>")
-  $("#weather").append("<b>In 21 Hours</b><br>" + result.list[7].weather[0].description + "<br>" + faren + " Degrees Farenheit <br><br>")
-  $("#weather").append("<b>In 24 Hours</b><br>" + result.list[8].weather[0].description + "<br>" + faren + " Degrees Farenheit <br><br>")
+  $("#weather").append("<b><h3>Location: " + city + " </h3>Now</b><br>" + result.list[0].weather[0].description + "<br>" + faren + "&deg; F <br><br>")
+  $("#weather").append("<b>In 3 Hours</b><br>" + result.list[1].weather[0].description + "<br>" + faren + "&deg; F <br><br>")
+  $("#weather").append("<b>In 6 Hours</b><br>" + result.list[2].weather[0].description + "<br>" + faren + "&deg; F <br><br>")
+  $("#weather").append("<b>In 9 Hours</b><br>" + result.list[3].weather[0].description + "<br>" + faren + "&deg; F <br><br>")
+  $("#weather").append("<b>In 12 Hours</b><br>" + result.list[4].weather[0].description + "<br>" + faren + "&deg; F <br><br>")
+  $("#weather").append("<b>In 15 Hours</b><br>" + result.list[5].weather[0].description + "<br>" + faren + "&deg; F <br><br>")
+  $("#weather").append("<b>In 18 Hours</b><br>" + result.list[6].weather[0].description + "<br>" + faren + "&deg; F <br><br>")
+  $("#weather").append("<b>In 21 Hours</b><br>" + result.list[7].weather[0].description + "<br>" + faren + "&deg; F <br><br>")
+  $("#weather").append("<b>In 24 Hours</b><br>" + result.list[8].weather[0].description + "<br>" + faren + "&deg; F <br><br>")
   
   }).fail(function(err) {
   throw err;
