303b1f3a7d298799475f71abed555216097584dd
ccho0415 <ccho0415@gmail.com>
2017-01-12 00:30:40 -0500

    adjusting news page css

--- assets/javascript/news.js
+++ assets/javascript/news.js
@@ -9,10 +9,10 @@ $(document).ready(function() {
 		$.ajax({url: nyt_url1, method: 'GET'}).done(function(result) {
 
 		  $(".s2a").html("")
-		  $(".s2a").append("<div class='row'><div class='col-md-1'></div> <div class='col-md-11'><h2>" + topic1 + "</h2></div></div>")
+		  $(".s2a").append("<div class='row'><div class='col-md-10'></div> <div class='col-md-12'><h2>" + topic1 + "</h2></div></div>")
 		  for (var i = 0; i <2; i++) {
 		    var current_result = result.results[i]
-		    $(".s2a").append("<div class='row'><div class='col-md-1'> <img src="+current_result.multimedia[0].url+"></div><div class='col-md-11'> <b> <a href="+ current_result.url + ">" + current_result.title + "</a></b><br>" + current_result.byline + "<br><i>" + current_result.abstract + "</i><br><br></div></div>")
+		    $(".s2a").append("<div class='row'><div class='col-md-2'> <img src="+current_result.multimedia[0].url+"></div><div class='col-md-10'> <b> <a href="+ current_result.url + ">" + current_result.title + "</a></b><br>" + current_result.byline + "<br><i>" + current_result.abstract + "</i><br><br></div></div>")
 		  }
 		  
 		  }).fail(function(err) {
@@ -23,10 +23,10 @@ $(document).ready(function() {
 		$.ajax({url: nyt_url2, method: 'GET'}).done(function(result) {
 
 		  $(".s2b").html("")
-		  $(".s2b").append("<div class='row'><div class='col-md-1'></div> <div class='col-md-11'><h2>" + topic2 + "</h2></div></div>")
+		  $(".s2b").append("<div class='row'><div class='col-md-1'></div> <div class='col-md-10'><h2>" + topic2 + "</h2></div></div>")
 		  for (var i = 0; i <2; i++) {
 		    var current_result = result.results[i]
-		    $(".s2b").append("<div class='row'></div> <div class='col-md-1'> <img src="+current_result.multimedia[0].url+"></div><div class='col-md-11'> <b> <a href="+ current_result.url + ">" + current_result.title + "</a></b><br>" + current_result.byline + "<br><i>" + current_result.abstract + "</i><br><br></div></div>")
+		    $(".s2b").append("<div class='row'></div> <div class='col-md'> <img src="+current_result.multimedia[0].url+"></div><div class='col-md-12'> <b> <a href="+ current_result.url + ">" + current_result.title + "</a></b><br>" + current_result.byline + "<br><i>" + current_result.abstract + "</i><br><br></div></div>")
 		  }
 		  
 		  }).fail(function(err) {
@@ -37,10 +37,10 @@ $(document).ready(function() {
 		$.ajax({url: nyt_url3, method: 'GET'}).done(function(result) {
 
 		  $(".s2c").html("")
-		  $(".s2c").append("<div class='row'><div class='col-md-1'></div> <div class='col-md-11'><h2>" + topic3 + "</h2></div></div>")
+		  $(".s2c").append("<div class='row'><div class='col-md-1'></div> <div class='col-md-12'><h2>" + topic3 + "</h2></div></div>")
 		  for (var i = 0; i <2; i++) {
 		    var current_result = result.results[i]
-		    $(".s2c").append("<div class='row'><div class='col-md-1'> <img src="+current_result.multimedia[0].url+"></div><div class='col-md-11'> <b> <a href="+ current_result.url + ">" + current_result.title + "</a></b><br>" + current_result.byline + "<br><i>" + current_result.abstract + "</i><br><br></div></div>")
+		    $(".s2c").append("<div class='row'><div class='col-md-1'> <img src="+current_result.multimedia[0].url+"></div><div class='col-md-12'> <b> <a href="+ current_result.url + ">" + current_result.title + "</a></b><br>" + current_result.byline + "<br><i>" + current_result.abstract + "</i><br><br></div></div>")
 		  }
 		  
 		  }).fail(function(err) {
@@ -51,10 +51,10 @@ $(document).ready(function() {
 		// $.ajax({url: nyt_url4, method: 'GET'}).done(function(result) {
 
 		//   $(".s2d").html("")
-		//   $(".s2d").append("<div class='row'><div class='col-md-1'></div> <div class='col-md-11'><h2>" + topic4 + "</h2></div></div>")
+		//   $(".s2d").append("<div class='row'><div class='col-md-1'></div> <div class='col-md-10'><h2>" + topic4 + "</h2></div></div>")
 		//   for (var i = 0; i <2; i++) {
 		//     var current_result = result.results[i]
-		//     $(".s2d").append("<div class='row'><div class='col-md-1'> <img src="+current_result.multimedia[0].url+"></div><div class='col-md-11'> <b> <a href="+ current_result.url + ">" + current_result.title + "</a></b><br>" + current_result.byline + "<br><i>" + current_result.abstract + "</i><br><br></div></div>")
+		//     $(".s2d").append("<div class='row'><div class='col-md-1'> <img src="+current_result.multimedia[0].url+"></div><div class='col-md-10'> <b> <a href="+ current_result.url + ">" + current_result.title + "</a></b><br>" + current_result.byline + "<br><i>" + current_result.abstract + "</i><br><br></div></div>")
 		//   }
 		  
 		//   }).fail(function(err) {
