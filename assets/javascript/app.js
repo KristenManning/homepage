4af3a4c2ed2601e4915f147e4f55d52654a96191
ccho0415 <ccho0415@gmail.com>
2017-01-12 09:59:56 -0500

    made a prettier music box. will add more css to music box later

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -28,3 +28,11 @@ function closeNav() {
   document.getElementById("wrapper").style.marginLeft = "0";
 }
 // NavBar JS Ends
+function openMusic(){
+	document.getElementById("musicbox").style.width = "250px";
+	document.getElementById("wrapper").style.marginRight = "250px";
+}
+function closeMusic() {
+  document.getElementById("musicbox").style.width = "0";
+  document.getElementById("wrapper").style.marginRight = "0";
+}
