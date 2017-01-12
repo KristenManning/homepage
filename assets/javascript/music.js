f7e1ce66022180a88342d7d5c38801aff87e51bf
ccho0415 <ccho0415@gmail.com>
2017-01-11 20:38:53 -0500

    scripts for music and toDo have to come after body. Music is there but it looks derpy... Need to fix the css on that

---
+++ assets/javascript/music.js
@@ -0,0 +1,19 @@
+$("#sSButton").on("click", function(){
+  let  q = $("#songSearch").val();
+    console.log(q)
+  var qA = "q=" + q + "&type=track&market=US"
+  var queryURL = "https://api.spotify.com/v1/search?"+ qA + "";
+  var music = "";
+  var audio = "";
+ $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+ .done(function(response){
+  console.log(response.tracks.items[0]);
+$("#artist").text(response.artists);
+  let music = response.tracks.items[0].artists[0].uri;
+  console.log(music);
+$("#music").html ("<iframe src='https://embed.spotify.com/?uri="+ music+"' width='300' height='380' frameborder='0' allowtransparency='true'></iframe>");
+ });
+  });
--- assets/javascript/toDo.js
+++ assets/javascript/toDo.js
@@ -1,1 +1,69 @@
 console.log("hello!")
+// Global Vars
+    var stickArr = [];
+    var count = 0;
+    var notes = $("#display");
+    // Binding Create Button
+    $("#create").on("click", function(){
+      // Append to the Sticky Note Display
+      $("#display").append("<li id = 'sticky'>"+
+          "<textarea class = 'noteTitle' placeholder='Title' maxlength='10'>"+
+          "</textarea><textarea class = 'noteContent' placeholder='Content'></textarea>"+
+          "<a href='javascript:void(0)' class = '' id = 'closebtn' onclick= 'close()' >&times;</a>"+
+          "</li>");
+    });
+    // Binding Save Button
+    $("#save").on("click", function(event){
+      // Preventing Double Clicking
+      event.preventDefault();
+      // Searching through each sticky note and grabbing data
+      notes.find("li").each(function (i, e) {
+        var title = $(e).find("textarea.noteTitle");
+        var content = $(e).find("textarea.noteContent");
+          stickArr.push({Index: i, Title: title.val(), Content: content.val()});
+        });
+        // Saving the Data
+      var jsonStr= JSON.stringify(stickArr);
+      localStorage.setItem("notes", jsonStr);
+
+    });
+    // Binding Delete Button
+    $(document).on("click", "#closebtn", function(){
+      $(this).closest("#sticky").remove();
+        return false;
+      });
+    // Part of Load Function
+    function add(title, content){
+      // Add notes
+      notes.append("<li id = 'sticky'>"+
+        "<textarea class = 'noteTitle' placeholder='Title' maxlength='10'>"+
+        "</textarea><textarea class = 'noteContent' placeholder='Content'></textarea>"+
+        "<a href='javascript:void(0)' class = '' id = 'closebtn' onclick= 'close()' >&times;</a>"+
+        "</li>");
+      // Parse through saved data
+      var newNote = notes.find("li:last");
+      if(title){
+        newNote.find("textarea.noteTitle").val(title);
+      }
+  if(content){
+    newNote.find("textarea.noteContent").val(content);
+  }
+
+}
+// Load Old Sticky Notes Function
+    function load (){
+      // Get data from local Storage
+      var stored= localStorage.getItem("notes");
+      // Store the data into a an Array
+        if (stored){
+          var stickArr = JSON.parse(stored);
+          count = stickArr.length
+          var i;
+          for (i=0; i < count; i ++){
+            var stored= stickArr[i];
+            add(stored.Title, stored.Content);
+          }
+        }
+    }
+// Calling Load Function
+    load();
