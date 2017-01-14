402962dfd1047abb883ac4e0e17da58f569f7bf7
ccho0415 <ccho0415@gmail.com>
2017-01-14 02:18:06 -0500

    changing landing page css

---
+++ assets/javascript/landingpage.js
@@ -0,0 +1,13 @@
+// $(document).ready(function(){
+// 	var queryURL = "https://pixabay.com/api/?key=4262426-23726f28b199ed68a981b7196&min_width=1080&image_type=photo"
+// 	$.ajax({
+// 		url: queryURL,
+// 		method: "GET"
+// 	})
+// 	.done(function(response){
+// 		console.log(response);
+// 		$("#landing").css("background-image", "url("+ ")")
+// 	})
+
+
+// })
